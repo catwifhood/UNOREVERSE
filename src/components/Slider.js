6c7ad17a456630adf16b3e67606c229c022e01de
LastSnowLeopard <forevergowith@gmail.com>
2024-06-04 14:19:40 -0400

    complete infinite loop of slider

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,30 +1,15 @@
-import React from 'react'
+import React from "react";
 
 export default function Slider() {
   return (
-    <div className='bg-[#328A10] py-2 md:py-5 flex gap-8 overflow-hidden'>
-      <h1 className='text-2xl md:text-4xl font-black'>$UNO</h1>
-      <h1 className='text-2xl md:text-4xl font-black'>$UNO</h1>
-      <h1 className='text-2xl md:text-4xl font-black'>$UNO</h1>
-      <h1 className='text-2xl md:text-4xl font-black'>$UNO</h1>
-      <h1 className='text-2xl md:text-4xl font-black'>$UNO</h1>
-      <h1 className='text-2xl md:text-4xl font-black'>$UNO</h1>
-      <h1 className='text-2xl md:text-4xl font-black'>$UNO</h1>
-      <h1 className='text-2xl md:text-4xl font-black'>$UNO</h1>
-      <h1 className='text-2xl md:text-4xl font-black'>$UNO</h1>
-      <h1 className='text-2xl md:text-4xl font-black'>$UNO</h1>
-      <h1 className='text-2xl md:text-4xl font-black'>$UNO</h1>
-      <h1 className='text-2xl md:text-4xl font-black'>$UNO</h1>
-      <h1 className='text-2xl md:text-4xl font-black'>$UNO</h1>
-      <h1 className='text-2xl md:text-4xl font-black'>$UNO</h1>
-      <h1 className='text-2xl md:text-4xl font-black'>$UNO</h1>
-      <h1 className='text-2xl md:text-4xl font-black'>$UNO</h1>
-      <h1 className='text-2xl md:text-4xl font-black'>$UNO</h1>
-      <h1 className='text-2xl md:text-4xl font-black'>$UNO</h1>
-      <h1 className='text-2xl md:text-4xl font-black'>$UNO</h1>
-      <h1 className='text-2xl md:text-4xl font-black'>$UNO</h1>
-      <h1 className='text-2xl md:text-4xl font-black'>$UNO</h1>
-      <h1 className='text-2xl md:text-4xl font-black'>$UNO</h1>
+    <div className="overflow-hidden max-w-[100vw] bg-[#328A10]">
+      <div className="py-2 md:py-5 flex gap-8 track-horizontal">
+        {Array(50)
+          .fill(null)
+          .map((item, key) => (
+            <h1 className="text-2xl md:text-4xl font-black">$UNO</h1>
+          ))}
+      </div>
     </div>
-  )
+  );
 }
