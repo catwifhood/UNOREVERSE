c85a7f212a3cbde095826fb41362578034a2e868
catwifhood <165341190+catwifhood@users.noreply.github.com>
2024-06-04 21:31:29 +0100

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -16,7 +16,7 @@ export default function Hero() {
             <a href='https://x.com/unoreverse_sol' target='_blank' rel="noreferrer">
               <img src="/x.png" alt="image" className="w-10 md:w-auto" />
             </a>
-            <a href="https://pump.fun/board" target="_blank" rel="noreferrer">
+            <a href="https://pump.fun/6tevznL4MszmRXZLFfTny8VSNUYWn6btQBpbhVd8pump" target="_blank" rel="noreferrer">
               <img src="/pill.png" alt="image" className="w-10 md:w-auto" />
             </a>
           </div>
