637d8959b753422b0aae9dd3d46ccf90c48e0273
catwifhood <165341190+catwifhood@users.noreply.github.com>
2024-06-04 21:11:30 +0100

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ export default function Header() {
     <div className='px-20 py-10 flex md:flex-row flex-col justify-between items-center bg-[#4C6B6E] gap-10 text-white'>
       <h1 className='text-2xl font-bold'>CA: COMING SOON</h1>
       <div className='flex-grow md:block hidden' />
-      <h1 className='text-2xl font-bold underline'>PUMP.FUN</h1>
+      <h1 className='text-2xl font-bold underline cursor-pointer' href='https://pump.fun/board' target='_blank' rel="noreferrer">PUMP.FUN</a>
       <a className='text-2xl font-bold underline cursor-pointer' href='https://t.me/unoreversesolana' target='_blank' rel="noreferrer">TELEGRAM</a>
       <a className='text-2xl font-bold underline cursor-pointer' href='https://x.com/unoreverse_sol' target='_blank' rel="noreferrer">TWITTER</a>
     </div>
