cdd56323afe79eaa76c214a0c6f7dfb6382c9a8a
LastSnowLeopard <forevergowith@gmail.com>
2024-06-04 13:46:53 -0400

    feat: reordered image

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -3,10 +3,10 @@ import React from 'react'
 export default function Gallery() {
   return (
     <div className='grid grid-cols-4'>
-      <div><img src='/gallery-2.png' alt='image' /></div>
       <div><img src='/gallery-1.png' alt='image' /></div>
-      <div><img src='/gallery-3.png' alt='image' /></div>
+      <div><img src='/gallery-2.png' alt='image' /></div>
       <div><img src='/gallery-4.png' alt='image' /></div>
+      <div><img src='/gallery-5.png' alt='image' /></div>
     </div>
   )
 }
